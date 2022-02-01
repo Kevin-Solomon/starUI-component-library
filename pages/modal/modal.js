230f14de81030755fa0e19f9ff10975d07cb0655
Kevin-Solomon <kevinsolomon404@gmail.com>
2022-02-01 17:24:08 +0530

    refactor : removed unnecessary console.log

--- pages/modal/modal.js
+++ pages/modal/modal.js
@@ -1,7 +1,6 @@
 const openModal = document.querySelector('#open-modal');
 const modal = document.querySelector('.modal');
 const modalContainer = document.querySelector('.modal-container');
-console.log(modal);
 openModal.addEventListener('click', ()=>{
   modalContainer.classList.add('show-container');
   modal.classList.add('show');
