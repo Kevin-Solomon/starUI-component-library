eac2457ed0e895ef658b62663f61538a2ca44c79
Kevin-Solomon <kevinsolomon404@gmail.com>
2022-02-01 12:39:32 +0530

    refactor : removed unwanted console.log and clearTimeout

--- pages/toast/toast.js
+++ pages/toast/toast.js
@@ -1,13 +1,9 @@
 const openToast = document.querySelector('#open-toast');
 const toast = document.querySelector('.toast')
-
-
-const clearToast = (timeout)=>{
+const clearToast = ()=>{
   toast.classList.remove('show');
-  clearTimeout(timeout)
 }
 openToast.addEventListener('click',()=>{
-  console.log('sike')
   toast.classList.add('show');
   setTimeout(clearToast,3000)
 })
