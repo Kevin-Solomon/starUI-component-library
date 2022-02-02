c5403092231b404c62cf0059eb97cc92ee0a81f3
Kevin-Solomon <kevinsolomon404@gmail.com>
2022-02-03 01:07:51 +0530

    feat : add hamburger variant to navigation page

--- pages/navigation/nav.js
+++ pages/navigation/nav.js
@@ -1,11 +1,8 @@
 const navbarBtn = document.querySelector('.navbar-btn');
 const collapsableContent = document.querySelector('.collapsable-content');
-console.log(collapsableContent)
-navbarBtn.addEventListener('click', ()=>{
-  if(collapsableContent.style.display === 'none'){
-    collapsableContent.style.display = 'block'
-  }
-  else{
-    collapsableContent.style.display = 'none'
-  }
+//explicitly stated this display of collapsableContent to be none 
+//So that javascipt does not return an empty string on the first call
+collapsableContent.style.display = 'none'
+navbarBtn.addEventListener('click', (e)=>{
+  collapsableContent.style.display === 'none' ? collapsableContent.style.display = 'block' : collapsableContent.style.display = 'none';
 })
